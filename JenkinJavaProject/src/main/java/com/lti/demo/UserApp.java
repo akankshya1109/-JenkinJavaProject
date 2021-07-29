73e73d0acc517cfefab079f654624ecadb909f73
akankshya1109 <ajayakumarswain65@gmail.com>
2021-07-29 21:25:17 +0530

    first commit

---
+++ JenkinJavaProject/src/main/java/com/lti/demo/UserApp.java
@@ -0,0 +1,7 @@
+package com.lti.demo;
+//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
+public class UserApp {
+ public static void main(String[] args) {
+	System.out.println("Jenkins Java Production");
+}
+}
